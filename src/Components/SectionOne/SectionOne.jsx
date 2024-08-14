0c5985b1a5642c822da3af8cb71fe97124daab28
NaglaFathi11 <naglafatehi11@gmail.com>
2024-08-14 18:32:54 +0200

    s

--- src/Components/SectionOne/SectionOne.jsx
+++ src/Components/SectionOne/SectionOne.jsx
@@ -1,13 +1,13 @@
-import './SectionOne.css'
-import UserImage from '../../assets/Images/avatar-jessica.jpeg'
+import "./SectionOne.css";
+import UserImage from "/Images/avatar-jessica.jpeg";
 
 export default function SectionOne() {
   return (
     <div id="section-one">
-        <img src={UserImage} alt="Not Found" />
-        <h1>Jessica Randall</h1>
-        <h2>London, United Kingdom</h2>
-        <p>“Front-end developer and avid reader.”</p>   
+      <img src={UserImage} alt="Not Found" />
+      <h1>Jessica Randall</h1>
+      <h2>London, United Kingdom</h2>
+      <p>“Front-end developer and avid reader.”</p>
     </div>
-  )
+  );
 }
